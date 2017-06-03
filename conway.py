b338becddde2361a87407209930e4de0c20b9436
Vivian Breda <vivianbreda@gmail.com>
2017-06-03 14:12:59 -0700

    Initial State
    Set up grid.

---
+++ conway.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+N = 4
+
+def addGlider(i, j, grid):
+    glider = np.array([[0, 0, 255],
+                       [255, 0, 255],
+                       [0, 255, 255]])
+    grid[i:i+3, j:j+3] = glider
+    
+grid = np.zeros(N*N).reshape(N,N)
+addGlider(1, 1, grid)
+
+#Random
+#x = np.random.choice([0,255], 4*4, p=[0.1, 0.9]).reshape(4, 4) 
+
+#Chosen
+#np.array([[0, 0, 255], [255, 255, 0], [0, 255, 0]])
+
+plt.imshow(grid, interpolation='nearest')
+plt.show()
